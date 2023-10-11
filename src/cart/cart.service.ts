812a2c4d7746ac953087d65cac50b394b9f49e34
amit kumar <amit83666@gmail.com>
2023-10-11 11:31:22 +0530

    remove console

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -29,67 +29,19 @@ export class CartService {
     return this.cartRepository.findOne({where:{id},relations:['cartItems']});
   }
 
-
-  // async addItemToCart(id:number,addItemDto:AddCartItemDto):Promise<CartItem>{
-  //   const user = await this.userRepository.findOne({where:{userId:id}, relations: ['carts'] });
-  //   console.log("uuuuuser ", user);
-  //   if (!user) {
-  //     throw new NotFoundException(`User with ID ${id} not found.`);
-  //   }
-  //   const product = await this.productRepository.findOne({where:{id:addItemDto.productId}});
-  //   if (!product) {
-  //     throw new NotFoundException(`Product with ID ${addItemDto.productId} not found.`);
-  //   }
-  //   console.log("product", product);
-
-  //   const cartItem = this.cartItemRepository.create({
-
-  //     product,
-  //     quantity: addItemDto.quantity,
-  //   });
-  //   await this.cartItemRepository.save(cartItem);
-  //   user.carts[0].cartItem.push(cartItem);
-  //   await this.cartRepository.save(user.carts[0]);
-    
-  // return cartItem;
-    // const addtoCart =  await this.cartRepository.findOne({where:{id:addItemDto.cartId}, relations:['cartItem'] })
-    // console.log("addtoCart", addtoCart.cartItem);
-
-
-    // const existingCartItem = user.cartItems.find((item) => item.product.id === addItemDto.productId);
-    // const existingCartItem = user.
-
-    // if (existingCartItem) {
-      // If the product is already in the cart, update the quantity
-    //   existingCartItem.quantity += addItemDto.quantity;
-    //   await this.cartItemRepository.save(existingCartItem);
-    //   return existingCartItem;
-    // } else {
-    //   // If the product is not in the cart, create a new cart item
-    //   const newCartItem = this.cartItemRepository.create({
-    //     user,
-    //     product,
-    //     quantity: addItemDto.quantity,
-    //   });
-
-    //   await this.cartItemRepository.save(newCartItem);
-    //   return newCartItem;
-    // return
-  // }
   async addItemToCart(cartId : number, addItemDto:AddCartItemDto):Promise<CartItem>{
     const cart = await this.cartRepository.findOne({where:{id:cartId}, relations: ['cartItems']});
     if (!cart) {
       throw new NotFoundException(`Cart with ID ${cartId} not found.`);
     }
-    console.log("cart", cart);
+  
     const product = await this.productRepository.findOne({where:{id:addItemDto.productId}});
     if (!product) {
       throw new NotFoundException(`Product with ID ${addItemDto.productId} not found.`);
     }
-    console.log("product", product);
-    const existingCartItem = cart.cartItems.find((item) => item.product.id === addItemDto.productId);
-    console.log("existingCartItem",existingCartItem);
-    
+  
+    const existingCartItem = cart.cartItems.find((item) =>item.id === addItemDto.productId);
+
     if(existingCartItem){
       existingCartItem.quantity += addItemDto.quantity;
       return this.cartItemRepository.save(existingCartItem);
